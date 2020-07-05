5789bb690afe91f4e4ec50612b9258904ba6f08c
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-05 14:04:34 -0300

    Criando as tabelas do bando de dados e organizando o arquivo de configurações

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy # É um modulo ORM. ORM é uma ferramenta que facilita a criação e comunicação com o banco de dados
+from flask_script import Manager # Modulo que muda a forma de execução da ferramenta e adiciona comandos a ela
+from flask_migrate import Migrate, MigrateCommand # Modulo que auxilia a migração do banco de dados
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.config.from_object('config')
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
